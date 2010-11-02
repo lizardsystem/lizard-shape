47d8601e409783007b6611fb3498449ec5509032
jackha <jackha@gmail.com>
2010-11-02 13:06:41 +0000

    add custom admin form validation

--- lizard_shape/admin.py
+++ lizard_shape/admin.py
@@ -1,10 +1,67 @@
 from django.contrib import admin
+from django import forms
 
 from lizard_shape.models import Category
 from lizard_shape.models import Shape
 from lizard_shape.models import ShapeLegend
 
 
+class ShapeNameError(Exception):
+    pass
+
+
+def check_extension_or_error(filename, extension, extension_name):
+    first = filename.rpartition('.')
+    if first[-1] != extension:
+        raise forms.ValidationError(
+            "%s doest not have extension .%s." % (extension_name, extension))
+
+
+class ShapeForm(forms.ModelForm):
+    """Admin form for model Shape. Adds extra checks.
+
+    TODO: How to test this class?"""
+
+    class Meta:
+        model = Shape
+
+    def clean(self):
+        cleaned_data = super(ShapeForm, self).clean()
+        shp_first = cleaned_data['shp_file'].name.rpartition('.')
+        dbf_first = cleaned_data['dbf_file'].name.rpartition('.')
+        shx_first = cleaned_data['shx_file'].name.rpartition('.')
+        prj_first = cleaned_data['prj_file'].name.rpartition('.')
+        if not(shp_first[0] == dbf_first[0] == shx_first[0] == prj_first[0]):
+            raise forms.ValidationError(
+                'Files do not have common filename base.')
+
+        return cleaned_data
+
+    def clean_shp_file(self, *args, **kwargs):
+        data = self.cleaned_data['shp_file']
+        check_extension_or_error(data.name, 'shp', 'Shp_file')
+        return data
+
+    def clean_dbf_file(self, *args, **kwargs):
+        data = self.cleaned_data['dbf_file']
+        check_extension_or_error(data.name, 'dbf', 'Dbf_file')
+        return data
+
+    def clean_shx_file(self, *args, **kwargs):
+        data = self.cleaned_data['shx_file']
+        check_extension_or_error(data.name, 'shx', 'Shx_file')
+        return data
+
+    def clean_prj_file(self, *args, **kwargs):
+        data = self.cleaned_data['prj_file']
+        check_extension_or_error(data.name, 'prj', 'Prj_file')
+        return data
+
+
+class ShapeAdmin(admin.ModelAdmin):
+    form = ShapeForm
+
+
 admin.site.register(Category)
-admin.site.register(Shape)
+admin.site.register(Shape, ShapeAdmin)
 admin.site.register(ShapeLegend)
