1b80749e87ab41ad5176105a350591008ed13cbe
Remco Gerlich <remco@gerlich.nl>
2011-11-04 14:22:57 +0000

    Fixed bug (function used in the view got an extra argument where it shouldn't)

--- lizard_shape/views.py
+++ lizard_shape/views.py
@@ -8,11 +8,13 @@ from django.template import RequestContext
 from lizard_shape.models import Category
 from lizard_map.views import AppView
 
-def shape_treeitems(cls, shapes):
+def shape_treeitems(shapes):
     """
     Make treeitems for given shapes, return them in a list.
     """
+    
     children = []
+
     for shape in shapes:
         # Append shapes by legend.
         shapelegends = shape.template.shapelegend_set.all()
@@ -48,6 +50,7 @@ def shape_treeitems(cls, shapes):
                  'type': 'shape',
                  'adapter_layer_json':
                  shapelegendclass.adapter_layer_json(shape)})
+
     return children
 
 def get_tree(parent=None):
@@ -55,7 +58,9 @@ def get_tree(parent=None):
     Make tree for homepage using Category and Shape.
     """
     result = []
+
     categories = Category.objects.filter(parent=parent)
+
     for category in categories:
         # Append sub categories.
         children = get_tree(parent=category)
@@ -65,8 +70,10 @@ def get_tree(parent=None):
                'type': 'category',
                'children': children}
         result.append(row)
+
     if parent is not None:
         result += shape_treeitems(parent.shapes.all())
+
     return result
 
 class HomepageView(AppView):
@@ -83,7 +90,7 @@ class HomepageView(AppView):
         """Get parameters from kwargs and GET parameters. TODO: better way"""
 
         self.root_slug = kwargs.get('root_slug', None)
-        
+
         return super(HomepageView, self).get(request, *args, **kwargs)
 
     def parent_category(self):
@@ -94,7 +101,7 @@ class HomepageView(AppView):
         return get_tree(self.parent_category())
 
     def crumbs(self):
-        # Do NOT add this as a class variable!
+        # Do not add this as a class variable.
         # It uses reverse(), which uses the urlconf, which uses this class. So at the time the class
         # is created, there are no urls yet and reverse() fails.
         return [ {'name': 'home', 'url': '/'},
