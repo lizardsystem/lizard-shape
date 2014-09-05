17d8b42305a6cbb6a842f9717729da6a5d413150
Remco Gerlich <remco@gerlich.nl>
2014-09-05 15:22:56 +0200

    No defaults in urls

--- lizard_shape/urls.py
+++ lizard_shape/urls.py
@@ -1,6 +1,6 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 from django.conf import settings
-from django.conf.urls.defaults import include, patterns, url
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
 
 from lizard_ui.urls import debugmode_urlpatterns
@@ -9,11 +9,11 @@ from lizard_shape.views import HomepageView
 urlpatterns = patterns(
     '',
     url(r'^$',
-     HomepageView.as_view(),
-     name='lizard_shape.homepage'),
+        HomepageView.as_view(),
+        name='lizard_shape.homepage'),
     url(r'^category/(?P<root_slug>.*)/$',
-     HomepageView.as_view(),
-     name='lizard_shape.homepage'),
+        HomepageView.as_view(),
+        name='lizard_shape.homepage'),
     )
 
 if getattr(settings, 'LIZARD_SHAPE_STANDALONE', False):
